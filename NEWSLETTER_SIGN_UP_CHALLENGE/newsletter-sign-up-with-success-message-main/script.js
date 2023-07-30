0ec96f665639b6cfc578bac60af515fe42a93751
Perle Rebeka <110097603+BecaMakesCodes@users.noreply.github.com>
2023-07-30 10:12:57 +0200

    Add files via upload

--- NEWSLETTER_SIGN_UP_CHALLENGE/newsletter-sign-up-with-success-message-main/script.js
+++ NEWSLETTER_SIGN_UP_CHALLENGE/newsletter-sign-up-with-success-message-main/script.js
@@ -1,17 +1,43 @@
-// Declare variables for the two containers and two buttons
-/*
+// Declare variables for the two containers and two buttons, and invalid label
+
 let hideContainer = document.querySelector("#hide_container");
 let revealButton = document.querySelector("#reveal_container");
 let hideButton = document.querySelector("#hide");
 let revealSuccessContainer = document.querySelector("#reveal_success_container");
+let invalidLabel = document.querySelector("#invalid");
 
-// Hide / Reveal container functions
-function HideContainerRevealSuccess(){
-    hideContainer.style.visibility = "hidden";
-    revealSuccessContainer.style.visibility = "visible";
+// Form validation declaration
+let inputField = document.querySelector("#form_input");
+let included =  inputField.value.includes("@");
+
+// Regex declaration
+let regex =  /^[a-zA-Z0-9]+@/g
+let isRegex = false;
+// Hide container if input field has content in it
+function HideContainerWhenValid(){
+   
+    if((inputField.value.match(regex))) {
+        isRegex = true; 
+        revealSuccessContainer.style.visibility = "visible";
+        hideContainer.style.visibility = "hidden"
+    }
+
+    else if(isRegex == false){
+        inputField.style.border = "1px solid red";
+        inputField.style.backgroundColor = "rgb(255,232,230)";
+        invalidLabel.style.visibility = "visible";
+    }
+
+    else if(inputField.value == ""){
+        inputField.style.border = "1px solid red";
+        inputField.style.backgroundColor = "rgb(255,232,230)";
+        invalidLabel.style.visibility = "visible";
+    }
+    
+    console.log(inputField.value);
 }
 
-hide.addEventListener("click", HideContainerRevealSuccess);
+hide.addEventListener("click", HideContainerWhenValid);
 
 function RevealContainerHideSuccess(){
     revealSuccessContainer.style.visibility = "hidden";
@@ -19,17 +45,5 @@ function RevealContainerHideSuccess(){
 }
 reveal_container.addEventListener("click", RevealContainerHideSuccess);
 
-// Form validation declaration
-*/
 
-let inputEmailByUser = document.forms["sub_form"]["input_form"].value;
-let inputField = document.querySelector("form_input");
 
-// Form validation function
-
-function FormValidation(){
-    if(inputEmailByUser === ""){
-        inputField.style.border = "1px solid red";
-    }
-}
-hide.addEventListener("click", FormValidation);
